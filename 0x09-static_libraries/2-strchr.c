333998ba2032cafded25886e529a693e615245f7
MichaelAfolabi <topsonmich1@gmail.com>
2023-04-17 08:17:41 -0700

    Static Library prototype

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
+ BIN +1.42 KB 
+0x09-static_libraries/2-strchr.o
