93cbb86ad9a274dda8d86ff627dedfd01a9a4e9d
Michafolab <topsonmich1@gmail.com>
2023-05-08 08:50:35 -0700

    program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <elf.h>
-
+#include "main.h"
 
 /**
  * print_addr - prints address
